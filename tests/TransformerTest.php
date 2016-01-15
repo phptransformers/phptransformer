962b92445cf8c589271183ec6fb03503eb5abd66
MacFJA <macfja@gmail.com>
2016-01-15 22:02:28 +0100

    Separate Twig and Smarty implementation (phptransformers/phptransformer#26)

--- tests/TransformerTest.php
+++ tests/TransformerTest.php
@@ -2,53 +2,89 @@
 
 namespace PhpTransformers\PhpTransformer\Test;
 
+use PhpTransformers\PhpTransformer\TransformerInterface;
+
+/**
+ * Generic Unit test for PhpTransformer and transformers
+ */
 class TransformerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Test of the function ->renderFile
+     *
      * @dataProvider engineProvider
+     * @param string $name Transformer short name / identifier
+     * @param string $className The transformer class name
+     * @param string $fqcn The transformer Full Qualifier Class Name
      */
-    public function testRenderFile($name, $className)
+    public function testRenderFile($name, $className, $fqcn)
     {
-        $class = 'PhpTransformers\\PhpTransformer\\' . $className;
+        $class = $fqcn;
+        /** @var TransformerInterface $engine */
         $engine = new $class();
         $template = "tests/Fixtures/$className.$name";
         $locals = array(
             'name' => 'Linus',
         );
         $actual = $engine->renderFile($template, $locals);
-        $this->assertEquals('Hello, Linus!', $actual);
+        self::assertEquals('Hello, Linus!', $actual);
     }
 
     /**
+     * Test of the function ->render
+     *
      * @dataProvider engineProvider
+     * @param string $name Transformer short name / identifier
+     * @param string $className The transformer class name
+     * @param string $fqcn The transformer Full Qualifier Class Name
      */
-    public function testRender($name, $className)
+    public function testRender($name, $className, $fqcn)
     {
-        $class = 'PhpTransformers\\PhpTransformer\\' . $className;
+        $class = $fqcn;
+        /** @var TransformerInterface $engine */
         $engine = new $class();
         $template = file_get_contents("tests/Fixtures/$className.$name");
         $locals = array(
             'name' => 'Linus',
         );
         $actual = $engine->render($template, $locals);
-        $this->assertEquals('Hello, Linus!', $actual);
+        self::assertEquals('Hello, Linus!', $actual);
     }
 
     /**
+     * Test of the function ->getName
+     *
      * @dataProvider engineProvider
+     * @param string $name Transformer short name / identifier
+     * @param string $className The transformer class name
+     * @param string $fqcn The transformer Full Qualifier Class Name
      */
-    public function testGetName($name, $className)
+    public function testGetName($name, $className, $fqcn)
     {
-        $class = 'PhpTransformers\\PhpTransformer\\' . $className;
+        $class = $fqcn;
+        /** @var TransformerInterface $engine */
         $engine = new $class();
-        $this->assertEquals($name, $engine->getName());
+        self::assertEquals($name, $engine->getName());
     }
 
+    /**
+     * The main data provider of the unit test.
+     * Return an array with the short name / identifier, the class name and the FQCN of multiple transformers
+     *
+     * @return array
+     */
     public function engineProvider()
     {
-        $tests[] = array('smarty', 'SmartyTransformer');
-        $tests[] = array('twig', 'TwigTransformer');
+        self::markTestSkipped('PhpTransformer need to be tagged');
+        $engines = array();
 
-        return $tests;
+        //$engines[] = array('smarty', 'SmartyTransformer', 'PhpTransformers\\Smarty\\SmartyTransformer');
+        //$engines[] = array('twig', 'TwigTransformer', 'PhpTransformers\\Twig\\TwigTransformer');
+        //$engines[] = array('latte', 'LatteTransformer', 'PhpTransformers\\Latte\\LatteTransformer');
+        //$engines[] = array('plates', 'PlatesTransformer', 'PhpTransformers\\Plates\\PlatesTransformer');
+        //$engines[] = array('string-template', 'StringTemplateTransformer', 'PhpTransformers\\StringTemplate\\StringTemplateTransformer');
+        //$engines[] = array('mustache', 'MustacheTransformer', 'PhpTransformers\\Mustache\\MustacheTransformer');
+
+        return $engines;
     }
 }
